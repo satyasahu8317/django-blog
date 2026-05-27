d072c5787e45dba45510605fecd1a350a56610d1
satyasahu8317 <sahusatya8317@gmail.com>
2026-05-27 14:23:39 +0530

    Complete UI overhaul with Tailwind CSS and set along as homepage

--- core/urls.py
+++ core/urls.py
@@ -25,7 +25,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',include('posts.urls',namespace='posts')),
+    path('',include('posts.urls',namespace='posts')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('summernote/', include('django_summernote.urls')),
     
